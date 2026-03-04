488c2f627948f0874ae127591427811df455cb26
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-03-04 20:16:14 -0300

    Create "tarea-2" anda point 1 ✔️

---
+++ tarea-2/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Registro } from './registro/registro';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Registro],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('tarea-2');
+}
