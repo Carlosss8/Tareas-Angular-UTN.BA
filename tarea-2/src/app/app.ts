e4d0ea97de25735d940427c8025462e4c9a18883
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-03-04 20:57:13 -0300

    fin point 2

--- tarea-2/src/app/app.ts
+++ tarea-2/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Registro } from './registro/registro';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Registro, ReactiveFormsModule],
+  imports: [RouterOutlet, Registro],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- tarea-2/src/app/registro/registro.ts
+++ tarea-2/src/app/registro/registro.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { email, validate } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registro.html',
   styleUrl: './registro.css',
 })
-export class Registro { }
+export class Registro {
+  form: FormGroup
+  usuarios: any[] = []
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nombre: ["", Validators.required, Validators.minLength(3)],
+      email: ["", Validators.required, Validators.email],
+      mensaje: ["", Validators.required]
+    })
+  }
+
+  enviar() {
+    if (this.form.valid) {
+      this.usuarios.push(this.form.value)
+      this.form.reset
+    }
+  }
+}
