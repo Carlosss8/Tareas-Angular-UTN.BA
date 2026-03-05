9bca92f00fc9d3fbf38ca1bd26ef6a9682afed2c
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-03-05 20:48:16 -0300

    new directivas point 3 ✔️

--- tarea-2/src/app/registro/registro.ts
+++ tarea-2/src/app/registro/registro.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 import { email, validate } from '@angular/forms/signals';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-registro',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass, NgStyle],
   templateUrl: './registro.html',
   styleUrl: './registro.css',
 })
@@ -12,18 +13,34 @@ export class Registro {
   form: FormGroup
   usuarios: any[] = []
 
+  errores: any = {
+    nombre: [
+      { tipo: "required", mensaje: "El nombre es obligatorio" },
+      { tipo: "minlength", mensaje: "El nombre debe tener al menos 3 caracteres" }
+    ],
+    email: [
+      { tipo: "required", mensaje: "El email es obligatorio" },
+      { tipo: "email", mensaje: "El email no tiene formato válido" }
+    ]
+  }
+
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
-      nombre: ["", Validators.required, Validators.minLength(3)],
-      email: ["", Validators.required, Validators.email],
-      mensaje: ["", Validators.required]
+      nombre: ["", [Validators.required, Validators.minLength(3)]],
+      email: ["", [Validators.required, Validators.email]],
+      mensaje: [""]
     })
   }
 
+  formEnviado = false
+  isError = true
   enviar() {
     if (this.form.valid) {
-      this.usuarios.push(this.form.value)
-      this.form.reset
+      this.usuarios.push(this.form.value);
+      this.form.reset();
+      this.formEnviado = true;
+    } else {
+      this.form.markAllAsTouched();
     }
   }
 }
